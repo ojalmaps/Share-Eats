897af081b73abccab1a53bcfc69292d1c0392e6b
Sharon Wong <26sswong@gmail.com>
2021-09-11 15:25:54 -0700

    Revert "save changes"
    This reverts commit 3ce783b67a277d6379cb3fa7462f8f22e279cc1b.

--- react-app/src/components/Navigation.js
+++ react-app/src/components/Navigation.js
@@ -52,6 +52,14 @@ function Navigation({ isAuthenticated, window }) {
             <div className={classes.toolbar} onClick={handleDrawerToggle} />
             <Divider />
             <List>
+                <ListItem button key='Profile' component={Link} to="/profile">
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary='Profile' />
+                </ListItem>
+                <ListItem button key='Feed' component={Link} to="/feed">
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary='Feed' />
+                </ListItem>
                 <ListItem button key='Recommendations' component={Link} to="/recommendationlist">
                     <ListItemIcon></ListItemIcon>
                     <ListItemText primary='Recommendations' />
